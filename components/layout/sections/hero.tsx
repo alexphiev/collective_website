74893a8ffc0bea5d6f33f536d575de4833ac9636
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-08-03 16:36:23 +0200

    Headline and theme test

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -16,22 +16,20 @@ export const HeroSection = () => {
             <span className="mr-2 text-primary">
               <Badge>New</Badge>
             </span>
-            <span> Design is out now! </span>
+            <span> Availabilities from September </span>
           </Badge>
 
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
-              Experience the
+              We build digital products with
               <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                Shadcn
+                impact 🌿
               </span>
-              landing page
             </h1>
           </div>
 
           <p className="max-w-screen-sm mx-auto text-xl text-muted-foreground">
-            {`We're more than just a tool, we're a community of passionate
-            creators. Get access to exclusive resources, tutorials, and support.`}
+            {`We're here to help you build digital products that make a difference.`}
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
