a19b9cd4397a4529d44490e9d74fe8ea06a624b9
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-08-06 13:59:20 +0200

    Hero section improvements

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -11,13 +11,13 @@ export const HeroSection = async ({ lng }: { lng: string }) => {
 
   return (
     <section className="container w-full">
-      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-28">
         <div className="text-center space-y-8">
-          <Badge variant="outline" className="text-sm py-2">
+          <Badge variant="outline" className="text-sm py-2 px-3">
             <span className="mr-2 text-primary">
-              <Badge>New</Badge>
+              <Badge>{t("hero.badge.label")}</Badge>
             </span>
-            <span> Availabilities from September </span>
+            <span> {t("hero.badge.title")} </span>
           </Badge>
 
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
@@ -37,21 +37,12 @@ export const HeroSection = async ({ lng }: { lng: string }) => {
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
             <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
-              Get Started
+              {t("hero.cta1")}
               <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
             </Button>
 
-            <Button
-              asChild
-              variant="secondary"
-              className="w-5/6 md:w-1/4 font-bold"
-            >
-              <Link
-                href="https://github.com/nobruf/shadcn-landing-page.git"
-                target="_blank"
-              >
-                Github respository
-              </Link>
+            <Button variant="secondary" className="w-5/6 md:w-1/4 font-bold">
+              {t("hero.cta2")}
             </Button>
           </div>
         </div>
