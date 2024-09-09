75e43a758381d7f3258760e5f2dcc04340193b12
daanknoors <d.knoors@gmail.com>
2024-09-09 12:12:36 +0200

    update picture daan

--- utils/team-utils.ts
+++ utils/team-utils.ts
@@ -1,6 +1,7 @@
 import { TFunction } from "i18next";
 import alexandre from "@/public/alexandre.jpeg";
 import bernardo from "@/public/bernardo_compressed.jpeg";
+import daan from "@/public/daan.jpeg";
 import { StaticImageData } from "next/image";
 
 interface TeamProps {
@@ -59,13 +60,13 @@ export const getTeam = (t: TFunction): TeamProps[] => {
       ],
     },
     {
-      imageUrl: "https://i.pravatar.cc/250?img=61",
+      imageUrl: daan,
       firstName: "Daan",
       lastName: "Knoors",
-      position: "Senior Data Scientist",
-      yearsOfExperience: 7,
+      position: "Senior Data Scientist & Privacy Engineer",
+      yearsOfExperience: 8,
       nationality: "nl",
-      languages: ["nl", "en", "es"],
+      languages: ["nl", "en", "es", "de"],
       socialNetworks: [
         {
           name: "LinkedIn",
