c8376d10ae987a4fce58485aa336e447f512ed07
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-09-25 12:44:12 +0200

    Added Kyrre's testimonial

--- utils/testimonials-utils.ts
+++ utils/testimonials-utils.ts
@@ -1,6 +1,7 @@
 import { TFunction } from "i18next";
 import jaime from "@/public/jaime.jpeg";
 import henrik from "@/public/henrik.jpeg";
+import kyrre from "@/public/kyrre.jpeg";
 import { StaticImageData } from "next/image";
 
 export interface ReviewProps {
@@ -8,24 +9,27 @@ export interface ReviewProps {
   name: string;
   title: string;
   comment: string;
-  rating: number;
 }
 
 export const getTestimonials = (t: TFunction): ReviewProps[] => {
   return [
     {
+      image: kyrre,
+      name: "Kyrre Wathne",
+      title: "CTO · Viva Labs",
+      comment: t("testimonials.kyrre"),
+    },
+    {
       image: jaime,
       name: "Jaime Mateo",
       title: "CTO · Bridge for Billions",
       comment: t("testimonials.jaime"),
-      rating: 5.0,
     },
     {
       image: henrik,
       name: "Henrik Holen",
       title: "CEO · Viva Labs",
       comment: t("testimonials.henrik"),
-      rating: 5.0,
     },
   ];
 };
