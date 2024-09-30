73755ddb20792e8aed3413d18a4e6254b14627d7
Stefano <stefano.ventrudo10@gmail.com>
2024-09-30 11:19:21 +0200

    Reduce top space in SectionDivider

--- components/layout/sections/section-divider.tsx
+++ components/layout/sections/section-divider.tsx
@@ -1,6 +1,6 @@
 export const SectionDivider = () => {
   return (
-    <div className="container py-[120px] ">
+    <div className="container py-[80px] ">
       <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-accent/30 to-transparent"></div>
     </div>
   );
